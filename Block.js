8077cfb665363c6a6a67b2848a6ebc0926233867
Abhx27 <68250765+Abhx27@users.noreply.github.com>
2021-02-19 12:37:56 -0600

    Update Block.js

--- Block.js
+++ Block.js
@@ -17,7 +17,7 @@ class Blocks extends BaseClass {
      else{
       World.remove(world, this.body)
        push();
-       this.Visiblity = this.Visiblity - 2000;
+       this.Visiblity = this.Visiblity - 255;
        tint(255,this.Visiblity);
        image(this.image, this.body.position.x, this.body.position.y, 50, 50);
        pop();
